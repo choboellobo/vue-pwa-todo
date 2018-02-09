b7c38876cb8fa9cf733448053e11c505ffc6b9c8
Eduardo <choboellobo@gmail.com>
2018-02-09 13:11:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+  @import url('../node_modules/animate.css/animate.css');
+</style>
---
+++ src/pages/login/login.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div
+    span soy el login
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style src="./login.scss" lang="scss" scoped></style>
