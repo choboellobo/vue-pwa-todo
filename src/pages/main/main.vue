87ce4f4cb29177f717a03497ee2b4dd5592b73b8
begoru <betibup3@gmail.com>
2018-02-24 15:58:44 +0100

    new list

--- src/pages/main/main.vue
+++ src/pages/main/main.vue
@@ -1,6 +1,15 @@
 <template lang="pug">
   div
     Navbar
+    .container
+       .row
+         .col.s12
+            ul.collection
+              li.collection-item.avatar
+                span.title Lista de la compra
+                P completados 3 de 14
+                a.secondary-content
+                  i.material-icons more_vert
     //out of viewport
     //Loading
 </template>
