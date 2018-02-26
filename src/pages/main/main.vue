2cea89f6c02d01262e814aa27e010ce62d66d157
Eduardo <choboellobo@gmail.com>
2018-02-26 13:47:50 +0100

    add list of wallets

--- src/pages/main/main.vue
+++ src/pages/main/main.vue
@@ -1,25 +1,109 @@
 <template lang="pug">
   div
     Navbar
-    .container
+    //- If there is more than 0 wallets
+    .container(v-if="wallets.length > 0")
        .row
          .col.s12
             ul.collection
-              li.collection-item.avatar
-                span.title Lista de la compra
-                P completados #[strong 3] de #[strong 14]
+              li.collection-item.avatar(v-for="wallet in wallets" )
+                span.title {{wallet.name}}
+                p Completados #[strong {{ wallet.tasks | taskDone }}] de #[strong {{ Object.keys(wallet.tasks).length }}]
                 a.secondary-content
                   i.material-icons more_vert
     Fab
       a.btn-floating.btn-large.indigo.darken-1
         i.material-icons add
-    //out of viewport
-    //Loading
-    EmptyContent
+
+    // Out of viewport
+    //- If there is not wallets
+    Loading(v-if="!wallets")
+    //- If there is not wallets or wallets length is equal to 0
+    EmptyContent(v-if="wallets.length == 0 || !wallets")
+
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
 export default {
+  filters: {
+    taskDone(tasks) {
+      let num = 0;
+      for(let index in tasks) {
+        if( tasks[index].completed ) num++
+      }
+      return num
+    }
+  },
+  computed: {
+    ...mapGetters(['getUserData']),
+    canAddWallet() {
+      return this.walletModel.name.length == 0
+    }
+  },
+  data() {
+    return {
+      db: null,
+      wallets: null,
+      modalAddWallet: null,
+      walletModel: {name: ''}
+    }
+  },
+  mounted(){
+    let user = this.getUserData.uid
+    this.db = this.$firebase.database();
+    this.db.ref('/users/'+ user +'/wallets')
+      .on('value', snapshot => {
+        this.wallets = []
+        let wallets = snapshot.val()
+        if(wallets) this.getUserWallets(wallets)
+      })
+  },
+  methods: {
+    goToWallet(wallet) {
+      this.$router.push({name: 'Wallet', params: {key: wallet.key}})
+    },
+    shareWallet(key) {
+      let url = window.location.origin + '/#/mix/' + key
+      if (navigator.share) {
+          navigator.share({
+            title: 'Compartir mi lista',
+            text: 'Quiero compartir mi lista contigo, pincha en el enlace para añadirte a ella.',
+            url: url
+        })
+      }
+    },
+    removeWallet(key) {
+      let confirm = window.confirm('Desea borrar')
+      if(confirm) {
+        this.db.ref('/wallets/'+ key).remove()
+        this.db.ref('/users/' + this.getUserData.uid + '/wallets').transaction(data => {
+          return data.filter(elem => elem != key)
+        })
+      }
+    },
+    getUserWallets(walletArray) {
+      walletArray.forEach(wallet => {
+        this.db.ref('/wallets/'+ wallet).once('value', snapshot => {
+            this.wallets.push(Object.assign({key: wallet}, snapshot.val()))
+        });
+      })
+    },
+    createWallet(){
+      let promiseWallet = this.db.ref('wallets').push({
+        name: this.walletModel.name,
+        created: new Date().toString()
+      })
+      let promiseUserWallet = promiseWallet.then(res => {
+        this.walletModel.name = ''
+        this.db.ref('/users/' + this.getUserData.uid +'/wallets').transaction(data => {
+          Array.isArray(data) ? data.push(res.key) :  data = [res.key]
+          return data
+        })
+        Promise.all([promiseWallet, promiseUserWallet]).then(resolve => this.$materialize.Modal.getInstance(this.modalAddWallet).close())
+      })
+    }
+  }
 }
 </script>
 
