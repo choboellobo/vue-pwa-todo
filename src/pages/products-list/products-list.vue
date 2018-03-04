29060eeabe650818579fb80c199f874cf781bcad
begoru <betibup3@gmail.com>
2018-03-04 21:28:24 +0100

    product list menu page list product

--- src/pages/products-list/products-list.vue
+++ src/pages/products-list/products-list.vue
@@ -1,7 +1,15 @@
 <template lang="pug">
   div
     Navbar(title="soy un título" :backButton="true")
-      
+    .container
+      .row
+        .col.s12
+          ul.collection
+            li.collection-item
+              label
+                input(type="checkbox")
+                span.completed Pan
+              a.secondary-content #[i.material-icons more_vert]
 </template>
 <script>
 export default {
