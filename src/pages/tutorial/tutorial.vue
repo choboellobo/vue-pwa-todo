2e85e5acb7ba317f3f05937c655d23b8ee304677
begoru <betibup3@gmail.com>
2018-04-12 16:12:22 +0200

    tutorial page and its contents created

--- src/pages/tutorial/tutorial.vue
+++ src/pages/tutorial/tutorial.vue
@@ -1,10 +1,34 @@
 <template lang="pug">
     div
-        h1 aquí
+        .carousel.carousel-slider
+            .carousel-fixed-item.center
+                img(src="/assets/pull.png")
+            .carousel-item.indigo.lighten-2
+                h5 Crea tus listas 
+                p Podrás crear listas que incluirán productos, recados, tareas... y compartirlas con otros usuarios, así como eliminarlas.
+            .carousel-item.indigo.lighten-1
+                h5 Añade contenido a tus listas
+                p Podrás añadir los productos, recados, tareas... de la temática de tu lista que después podrás editar y/o eliminar.
+            .carousel-item.indigo.darken-1
+                h5 Aviso importante
+                p Recuerda que tu aplicación se actualizará automáticamente sin necesidad de que hagas nada. Que la difrutes!
 </template>
+
+<style lang="scss" src="./tutorial.scss" scoped>
+
+</style>
 
 <script>
 export default {
+    mounted(){
+        const elem = document.querySelector(".carousel")
+        const options = {
+            fullWidth: true,
+            indicators: true,
+           }
+        let carousel = this.$materialize.Carousel.init(elem,options)
+
+    }
   
 }
 </script>
