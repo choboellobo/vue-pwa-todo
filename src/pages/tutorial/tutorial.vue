d43fa1b61aaa145fb0055e0a5acb158104a1aae1
begoru <betibup3@gmail.com>
2018-04-15 21:21:47 +0200

    carousel tutorial edited

--- src/pages/tutorial/tutorial.vue
+++ src/pages/tutorial/tutorial.vue
@@ -2,14 +2,17 @@
     div
         .carousel.carousel-slider
             .carousel-fixed-item.center
-                img(src="/assets/pull.png")
+                button.btn.waves-effect.white.black-text EMPEZAR           
             .carousel-item.indigo.lighten-2
+                img
                 h5 Crea tus listas 
                 p Podrás crear listas que incluirán productos, recados, tareas... y compartirlas con otros usuarios, así como eliminarlas.
             .carousel-item.indigo.lighten-1
+                img
                 h5 Añade contenido a tus listas
                 p Podrás añadir los productos, recados, tareas... de la temática de tu lista que después podrás editar y/o eliminar.
             .carousel-item.indigo.darken-1
+                img
                 h5 Aviso importante
                 p Recuerda que tu aplicación se actualizará automáticamente sin necesidad de que hagas nada. Que la difrutes!
 </template>
