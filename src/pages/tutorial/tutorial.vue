5c1125f18cdc2e67b817840b32f2d8631408abc8
begoru <betibup3@gmail.com>
2018-04-20 16:22:26 +0200

    sliders tutorial page solucionated

--- src/pages/tutorial/tutorial.vue
+++ src/pages/tutorial/tutorial.vue
@@ -3,20 +3,21 @@
         .carousel.carousel-slider
             .carousel-fixed-item.center
                 button.btn.waves-effect.white.black-text EMPEZAR           
-            .carousel-item.slider-1
+            .carousel-item.slide1
+                img(src="static/img/icons/slider1.png")
                 h5 Crea tus listas 
                 p Podrás crear listas que incluirán productos, recados, tareas... y compartirlas con otros usuarios, así como eliminarlas.
-            .carousel-item.slider-2
+            .carousel-item.slide2
                 img(src="static/img/icons/slider2.png")
                 h5 Añade contenido a tus listas
                 p Podrás añadir los productos, recados, tareas... de la temática de tu lista que después podrás editar y/o eliminar.
-            .carousel-item.slider-3
+            .carousel-item.slide3
                 img(src="static/img/icons/slider3.png")
                 h5 Aviso importante
                 p Recuerda que tu aplicación se actualizará automáticamente sin necesidad de que hagas nada. Que la difrutes!
 </template>
 
-<style src="./tutorial.scss" lang="scss" scoped>
+<style lang="scss" src="./tutorial.scss" scoped>
 
 </style>
 
